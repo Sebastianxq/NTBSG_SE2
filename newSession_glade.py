370eea87ef57a07770fbd47792a5452f998baa45
Sebastianxq <Sxquinones@miners.utep.edu>
2018-10-18 08:59:04 -0600

    Update newSession_glade.py
    RD for figure 8 (missing format/connectivity)

--- newSession_glade.py
+++ newSession_glade.py
@@ -5,7 +5,7 @@ from gi.repository import Gtk
 builder = Gtk.Builder()
 builder.add_from_file("newSession_GUI.glade")
 
-window = builder.get_object("window1")
+window = builder.get_object("window1") 
 window.show_all()
 
 Gtk.main()
