da6ac62f096c78fe9d622bf85a603f8684c0a37f
PeterHanson5 <pghanson@miners.utep.edu>
2018-12-11 19:59:00 -0700

    Add files via upload

---
+++ Project4311/Field.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  8 19:28:20 2018
+
+@author: peter
+"""
+
+class field:
+    def __init__(self, fieldName, fieldShowName, fieldPosition, fieldSize, fieldValue, fieldShow):
+        self.fieldName = fieldName
+        self.fieldShowName = fieldShowName
+        self.fieldPosition = fieldPosition
+        self.fieldSize = fieldSize
+        self.fieldValue = fieldValue
+        self.fieldShow = fieldShow
+        
+    def edit():
+        
+    
