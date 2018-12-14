5ae1596b947df4ab33d5d6e33108b8b69c04d0e6
gmfelix <gmfelix@miners.utep.edu>
2018-12-14 01:00:01 -0700

    Getting ready for stage 4 integration

--- NTBSG.py
+++ NTBSG.py
@@ -1,14 +1,21 @@
 import sys
 from Helpers.pcapToPdml import *
 from SequencingStageFiles.stateMachine import StateMachine
+import re
 #sys.argv[1] = dissectorPath
 #sys.argv[2] = pcapPath
 #sys.argv[3] = workspacePath
-print(sys.argv[1])
-print(sys.argv[2])
-print(sys.argv[3])
+#print(sys.argv[1])
+#print(sys.argv[2])
+#print(sys.argv[3])
 
 nodeList = nodeSequenceFromPDML(sys.argv[1], sys.argv[2], sys.argv[3])
 fullInfo = fullInformationFromPDML(sys.argv[1], sys.argv[2], sys.argv[3])
 
 currStateMachine = StateMachine(nodeList)
+
+result = re.split('\\\\', sys.argv[3])
+resultLength = len(result)
+workspaceFolderName = result[resultLength-2]
+
+#print(workspaceFolderName)
