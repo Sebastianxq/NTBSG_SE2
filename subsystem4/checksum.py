1057c93cc6b3779a87f25b0bc2f5c6fd2abd7364
lanavar <lanavar@miners.utep.edu>
2018-12-13 21:56:36 -0700

    Added testing for checksum and corrected issue

--- subsystem4/checksum.py
+++ subsystem4/checksum.py
@@ -15,11 +15,28 @@ class Checksum():
     # Adding a relationship as long as it is not in the list
     def addRelationship(self, fn):
         if not fn in self.FieldName:
-            self.addFieldName(self, fn)
+            self.addFieldName(fn)
+        print("Added relationship")
+        self.testPrint()
 
     # Removing a relationship if it is in the list
     def removeRelationship(self, fn):
-        for fn in self.FieldName:
-            self.FieldName.remove(fn)
+        if fn in self.FieldName:
+            x = self.FieldName.index(fn)
+            print(x)
+            del self.FieldName[x]
 
-                      
+    # For testing print a
+    def testPrint(self):
+        print(self.PacketName)
+        print(self.FieldName)
+
+chk = Checksum("test")
+print chk.testPrint()
+chk.addRelationship("123")
+chk.addRelationship("234")
+chk.addRelationship("456")
+chk.testPrint()
+chk.removeRelationship("no")
+chk.testPrint()
+print("Done")
