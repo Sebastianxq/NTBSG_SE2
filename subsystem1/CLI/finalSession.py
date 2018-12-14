beeb1b02258ed51aa0132ba83cf242adf6196ded
Sebastianxq <Sxquinones@miners.utep.edu>
2018-12-14 12:01:29 -0700

    Cleaned code

--- subsystem1/CLI/finalSession.py
+++ subsystem1/CLI/finalSession.py
@@ -1,8 +1,6 @@
-from finalPCAP import PCAP
-from finalMessageType import messageType
-from finalFilter import filter
-import subprocess
-import xml.etree.ElementTree as ET
+from finalPCAP import PCAP #PCAP class obj
+from finalMessageType import messageType #messageType class obj
+from finalFilter import filter #filter class obj
 
 class Session:
 
@@ -12,42 +10,28 @@ class Session:
         self.description = description
         self.filterList = list()
         self.messageTypeList  = list()
+        self.fieldList = list()
 
-    #Binds the given PDML to the session object
-    #placeholder until we figure out how this works lol
     def addPDML(self,pdmlObject):
         self.sessionPDML = pdmlObject
 
-    #add a messageType object to the session
     def addMessageType(self,messageType):
         self.messageTypeList.append(messageType)
 
-    #add a filter object to the session
     def addFilter(self, filter):
         self.filterList.append(filter)
 
+    def addField(selfself, field):
+        self.fieldList.append(field)
+
+#creates a temp pcap, pdml, filter, messageType and session Objects
 testPcap = PCAP("testPcap", "no file")
-testPcap.convertToPdml()
-
-testFilter = filter("ipx only", "ipx")
-
-testMessageType = messageType("nameIsTest","name=test")
-
-#testPcap.convertToPdml()
-#print testPcap
-
-testSession = Session("test","A session used to showcase CLI functionality")
 pdml = testPcap.convertToPdml()
+testFilter = filter("ipx only", "ipx")
+testMessageType = messageType("nameIsTest","name=test")
+testSession = Session("test","A session used to showcase CLI functionality")
+
+#Binds pdml,filter and messageType to the session
 testSession.addPDML(pdml)
 testSession.addFilter(testFilter)
 testSession.addMessageType(testMessageType)
-
-#print testSession.sessionPDML
-print testSession.description
-print testSession.filterList
-print testSession.SessionName
-print testSession.messageTypeList
-
-
-
-#print testSession
