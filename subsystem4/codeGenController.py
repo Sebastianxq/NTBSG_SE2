03842590388a17de040359ef382d78ce3bd6a3ce
lanavar <lanavar@miners.utep.edu>
2018-12-14 12:28:07 -0700

    Connected controller to message template

--- subsystem4/codeGenController.py
+++ subsystem4/codeGenController.py
@@ -3,6 +3,12 @@
 # Class for the controller for the code genration controller
 
 import sys
+from checksum import Checksum
+from fieldLengthD import FieldLengthDependency
+from fieldEquv import FieldEquivelancy
+from packetLengthD import PacketLengthDependency
+from mesgtmp import MessageTemplate
+
 
 class CodeGenController():
     # Initializing the class it takes the path, and output format
@@ -13,37 +19,68 @@ class CodeGenController():
         self.sourcePN = ""
         self.targetPN = ""
         self.nodeDic = {}
+        self.sourceChksm = ""
+        self.targetChksm = ""
+        self.sourcePLD = ""
+        self.targetPLD = ""
+        self.MessagTmpl = ""
+        self.DestFN = "test"
+        self.code = {}
     # Method for obtaining the source and destination Message types
     def getPacketNames(self, seqInfo):
         self.sourcePN = seqInfo[0]
         self.targetPN = seqInfo[1]
+        self.getChecksum()
+        self.sourcePLD = self.getPLD(self.sourcePN)
+        self.targetPLD = self.getPLD(self.targetPN)
+        
 
     # Method for obtaining the dictionary
     def getDictionary(self, dic):
         self.nodeDic = dic
+        sourceDic = self.nodeDic[self.sourcePN]
+        targetDic = self.nodeDic[self.targetPN]
+        flen = 0
+        fn = ""
+        for key, val in sourceDic.items():
+            flen = len(value)
+            fn = key
+            self.sourcePLD.addFieldName(fn, flen)
+        for key, val in targetDic.items():
+            flen = len(value)
+            fn = key
+            self.targetPLD.addFieldName(fn, flen)
 
     # Method for setting checksum
     def setChecksum(self):
-        # Create checksum object get values
+        self.sourceChksm = Checksum(sourcePN)
+        self.targetChksm = Checksum(targetPN)
 
     # Method for getting packet length dependency
-    def setPLD(self):
-        #  Set the packet length dependencies
+    def setPLD(self, pn):
+        tmp = PacketLengthDependency(pn)
+        return tmp
 
     # Method for setting the field length dependencies
-    def setFLD(self):
-        # Set the field length dependencies
+    def setFLD(self, sfn, tfn):
+        fieldLd = FieldLengthDependency(sfn, tfn)
     
     # Method for setting the field Equivelancies
-    def setFE(self):
-        # Set the field lequivelancies
+    def setFE(self, sfn, smt, tfn, tmt):
+        fieldLE = FieldEquivelancy(sfn, smt, tfn, tmt)
+        
         
     # Method for generating and holding the code
     def generateCode(self):
-        # Pass info to generate code
-
+        self.MessagTmpl = MessageTemplate(self.DestFileName, self.DestFN, self.DestPath, self.OutputFormat)
+        self.MessagTmpl.collectPLD(self.sourcePN, self.targetPN)
+        self.MessagTmpl.generateCode()
+        self.code = self.MessagTmpl.retCode()
+        
+        
     # Method for creating code file
     def exportCode(self):
-        # Export and get copy of code
+        self.MessagTmpl.codeFile()
+        
 
     
